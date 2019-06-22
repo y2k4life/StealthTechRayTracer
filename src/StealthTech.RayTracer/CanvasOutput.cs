5bf81360a1dd16eae543b8e70d969937b8c3aed1
Guy Boicey <comps4food@live.com>
2019-06-22 13:16:15 -0700

    Add ability to save PPM, open PPM with associated app

---
+++ src/StealthTech.RayTracer/CanvasOutput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace StealthTech.RayTracer
+{
+    public class PpmOutput
+    {
+        public static void WriteToFile(string fileName, string ppmContent)
+        {
+            File.WriteAllText(fileName, ppmContent);
+            new Process
+            {
+                StartInfo = new ProcessStartInfo(fileName)
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
+        }
+    }
+}
